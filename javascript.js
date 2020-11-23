5d478c85c77d3d6b85311500858e83b521411f7d
andriesdeklerck <48009097+andriesdeklerck@users.noreply.github.com>
2020-11-23 21:47:20 +0100

    Cookies used

--- javascript.js
+++ javascript.js
@@ -1,7 +1,50 @@
 document.getElementById('btn').addEventListener('click', loadJson);
+document.getElementById('new').addEventListener('click', checkCookie);
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+    //   alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
+    loadJson();
+  }
 
 function loadJson() {
-    fetch('names.json')
+    var filename = "names";
+    var user=getCookie("username");
+    if (user != "") {
+      filename = "welcomeBack";
+    }
+
+    fetch(filename + '.json')
         .then((response) => {
             return response.json();
         })
@@ -10,9 +53,8 @@ function loadJson() {
             for (let index = 0; index < data.length; index++) {
                 console.log(data[index].Id)
             }
-
-            var tbl = document.createElement('table');
-            tbl.setAttribute('id', 'tabel');
+            document.getElementById("tabel").innerHTML = "";
+            var tbl = document.getElementById('tabel');
             var orderArrayHeader = ["ID", "Tree", "Date and Time"];
             var thead = document.createElement('thead');
             tbl.appendChild(thead);
@@ -34,7 +76,7 @@ function loadJson() {
                         td2.appendChild(document.createTextNode(data[i].DateTime));
                     }
                 }
-                document.getElementById("result").appendChild(tbl);
+                //document.getElementById("tabel").appendChild(tbl);
             }
         })
         .catch((err) => {
